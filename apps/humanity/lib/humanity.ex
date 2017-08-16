38c1743cba3e92ff35c8711cf229e9bac3b6a7f6
Richard Foster <richard@madetech.co.uk>
2017-08-16 19:29:51 +0100

    Add humanity app

---
+++ apps/humanity/lib/humanity.ex
@@ -0,0 +1,18 @@
+defmodule Humanity do
+  @moduledoc """
+  Documentation for Humanity.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Humanity.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/humanity/test/humanity_test.exs
@@ -0,0 +1,8 @@
+defmodule HumanityTest do
+  use ExUnit.Case
+  doctest Humanity
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
