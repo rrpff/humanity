03570ede8a66eeef20ee68d6d8aa4a046ea3aa62
Richard Foster <richard@madetech.co.uk>
2017-08-17 10:41:00 +0100

    Implement starting a humanity game and dealing cards

--- apps/humanity/lib/humanity.ex
+++ apps/humanity/lib/humanity.ex
@@ -1,18 +1,55 @@
 defmodule Humanity do
-  @moduledoc """
-  Documentation for Humanity.
-  """
+  defmodule GameState do
+    defstruct [:white_deck, :black_deck, :players, :state]
+  end
 
-  @doc """
-  Hello world.
+  defmodule Player do
+    defstruct [:name, :hand]
+  end
 
-  ## Examples
+  def init(players: players, white_cards: white_cards, black_cards: black_cards) do
+    game = %GameState{
+      white_deck: create_deck(white_cards),
+      black_deck: create_deck(black_cards),
+      players: Enum.map(players, &(create_player(name: &1))),
+      state: :ready
+    }
 
-      iex> Humanity.hello
-      :world
+    {:ok, game}
+  end
 
-  """
-  def hello do
-    :world
+  def deal_cards(game_state) do
+    {new_players, new_black_deck} = Enum.reduce(game_state.players, {[], game_state.black_deck}, fn (player, {new_players, new_black_deck}) ->
+      {new_player, new_black_deck} = fill_hand(player, new_black_deck)
+      new_players = Enum.concat(new_players, [new_player])
+      {new_players, new_black_deck}
+    end)
+
+    new_state = %GameState{game_state | players: new_players, black_deck: new_black_deck}
+
+    {:ok, new_state}
+  end
+
+  defp fill_hand(player, black_cards) do
+    num_required_cards = 2
+    num_player_cards = Enum.count(player.hand)
+    num_remaining_cards = num_required_cards - num_player_cards
+
+    new_player_cards = Enum.take(black_cards, num_remaining_cards)
+    new_black_cards = Enum.drop(black_cards, num_remaining_cards)
+    new_hand = Enum.concat(player.hand, new_player_cards)
+
+    {%Player{player | hand: new_hand}, new_black_cards}
+  end
+
+  defp create_deck(cards) do
+    Enum.shuffle(cards)
+  end
+
+  defp create_player(name: name) do
+    %Player{
+      name: name,
+      hand: []
+    }
   end
 end
--- apps/humanity/test/humanity_test.exs
+++ apps/humanity/test/humanity_test.exs
@@ -1,8 +1,67 @@
 defmodule HumanityTest do
-  use ExUnit.Case
-  doctest Humanity
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "creating a new game" do
+    setup do
+      {:ok, game} = Humanity.init(
+        players: ["Dana"],
+        white_cards: [1, 2, 3, 4, 5],
+        black_cards: ["a", "b", "c", "d", "e"]
+      )
+
+      %{game: game}
+    end
+
+    test "creates players", %{game: game} do
+      assert List.first(game.players) == %Humanity.Player{name: "Dana", hand: []}
+    end
+
+    test "creates a white deck", %{game: game} do
+      assert Enum.sort(game.white_deck) == [1, 2, 3, 4, 5]
+    end
+
+    test "creates a black deck", %{game: game} do
+      assert Enum.sort(game.black_deck) == ["a", "b", "c", "d", "e"]
+    end
+
+    test "shuffles the decks", %{game: game} do
+      {:ok, dup_game} = Humanity.init(
+        players: ["Dana"],
+        white_cards: [1, 2, 3, 4, 5],
+        black_cards: ["a", "b", "c", "d", "e"]
+      )
+
+      assert game.white_deck != dup_game.white_deck
+      assert game.black_deck != dup_game.black_deck
+    end
+
+    test "marks the game as ready", %{game: game} do
+      assert game.state == :ready
+    end
+  end
+
+  describe "dealing cards" do
+    setup do
+      {:ok, game} = Humanity.init(players: ["Dana", "Fox"], white_cards: [], black_cards: ["a", "b", "c", "d", "e"])
+      {:ok, game} = Humanity.deal_cards(game)
+
+      %{game: game}
+    end
+
+    test "deals cards to each player", %{game: game} do
+      assert Enum.map(game.players, &(&1.name)) == ["Dana", "Fox"]
+      assert Enum.flat_map(game.players, &(&1.hand)) |> Enum.count == 4
+    end
+
+    test "removes dealt cards from the black deck", %{game: game} do
+      assert Enum.count(game.black_deck) == 1
+    end
+
+    test "deals unique cards to each player", %{game: game} do
+      players_cards = game.players |> Enum.flat_map(&(&1.hand))
+      remaining_deck = game.black_deck
+
+      assert Enum.concat(players_cards, remaining_deck) |> Enum.uniq |> Enum.count == 5
+    end
   end
 end
